bbfafe88fd10b2cf591898c0e923eec30cdf316a
Guillaume Doussin <guillaume.doussin@amadeus.com>
2024-03-01 10:48:30 +0100

    Back to app 1.0

--- demo-app/src/main/java/com/agroprojects/PetResource.java
+++ demo-app/src/main/java/com/agroprojects/PetResource.java
@@ -18,25 +18,25 @@ public class PetResource {
 
         // Version 4
         // random 20% chance of failure
-        if (Math.random() * 100  < 20) {
-            throw new RuntimeException("Random failure");
-        }
+        //if (Math.random() * 100  < 20) {
+        //    throw new RuntimeException("Random failure");
+        //}
 
         // Version 3
         // random 33% chance of Dog/Cat/Bird
-        int randomInt = randomGenerator.nextInt(3);
-        if (randomInt == 0) {
-            return "Dog";
-        } else if (randomInt == 1) {
-            return "Cat";
-        } else {
-            return "Bird";
-        }
+        // int randomInt = randomGenerator.nextInt(3);
+        // if (randomInt == 0) {
+        //     return "Dog";
+        // } else if (randomInt == 1) {
+        //     return "Cat";
+        // } else {
+        //     return "Bird";
+        // }
 
         // Version 2
         //return "Wolf";
 
         // Version 1
-        //return "Dog";
+        return "Dog";
     }
 }
